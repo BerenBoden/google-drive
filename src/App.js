4dd8b67f39c15d25f118a786c22c8abdd2c2854a
Beren Boden <beren.boden@gmail.com>
2021-02-08 10:04:45 +1300

    added header component | added user state and passed as props to header component | added sidebar component | created media folder and added google drive logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "../src/components/header";
+import Sidebar from "../src/components/sidebar";
 
 function App() {
+  //Authentication with Firebase
+  const [user, setUser] = useState({
+    displayName: "Beren Boden",
+    email: "beren.boden@gmail.com",
+    emailVerified: true,
+    phoneNumber: null,
+    photoURL:
+      "https://yt3.ggpht.com/yti/ANoDKi7Q6BTSUGkX5Y9CrCk6bI1GmpXpqWMFpZ38p4rElw=s88-c-k-c0x00ffffff-no-rj-mo",
+  });
+
   return (
     <div className="App">
+      <Header userPhoto={user.photoURL} />
+      <Sidebar />
+      {/** If user authenticated: Show sidebar & files */}
 
+      {/** If user not authenticated: Log in */}
     </div>
   );
 }
